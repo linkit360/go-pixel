24e681f3a4991376edfeb117915ef50935591207
Helena Nevskaya <helena.nevskaya@ya.ru>
2016-11-07 18:57:53 +0300

    added notifier and api

---
+++ src/notifier/notifier.go
@@ -0,0 +1,80 @@
+package notifier
+
+import (
+	"encoding/json"
+	"fmt"
+
+	log "github.com/Sirupsen/logrus"
+
+	"github.com/vostrok/rabbit"
+)
+
+type Pixel struct {
+	Tid            string  `json:"tid,omitempty"`
+	Msisdn         string  `json:"msisdn,omitempty"`
+	CampaignId     int64   `json:"campaign_id,omitempty"`
+	SubscriptionId int64   `json:"subscription_id,omitempty"`
+	OperatorCode   int64   `json:"operator_code,omitempty"`
+	CountryCode    int64   `json:"country_code,omitempty"`
+	Pixel          string  `json:"pixel,omitempty"`
+	Publisher      string  `json:"publisher,omitempty"`
+	ResponseCode   int     `json:"response_code,omitempty"`
+	Took           float64 `json:"took,omitempty"`
+	Sent           bool    `json:"sent"`
+}
+
+type Notifier interface {
+	PixelNotify(msg Pixel) error
+}
+
+type NotifierConfig struct {
+	Queue queues            `yaml:"queue"`
+	Rbmq  rabbit.RBMQConfig `yaml:"rbmq"`
+}
+type queues struct {
+	PixelsQueue string `default:"pixels" yaml:"pixels"`
+}
+
+type notifier struct {
+	q  queues
+	mq rabbit.AMQPService
+}
+
+type EventNotify struct {
+	EventName string      `json:"event_name,omitempty"`
+	EventData interface{} `json:"event_data,omitempty"`
+}
+
+func init() {
+	log.SetLevel(log.DebugLevel)
+}
+
+func NewNotifierService(conf NotifierConfig) Notifier {
+	var n Notifier
+	{
+		rabbit := rabbit.NewPublisher(conf.Rbmq, rabbit.InitMetrics("mt_manager"))
+		n = &notifier{
+			q: queues{
+				PixelsQueue: conf.Queue.PixelsQueue,
+			},
+			mq: rabbit,
+		}
+	}
+	return n
+}
+
+func (service notifier) PixelNotify(msg Pixel) error {
+	log.WithField("pixel", fmt.Sprintf("%#v", msg)).Debug("got pixel")
+
+	event := EventNotify{
+		EventName: "pixels",
+		EventData: msg,
+	}
+	body, err := json.Marshal(event)
+	if err != nil {
+		return fmt.Errorf("json.Marshal: %s", err.Error())
+	}
+	log.WithField("body", string(body)).Debug("sent body")
+	service.mq.Publish(rabbit.AMQPMessage{service.q.PixelsQueue, body})
+	return nil
+}
---
+++ src/service/api.go
@@ -0,0 +1,130 @@
+package service
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+
+	"github.com/gin-gonic/gin"
+	rec "github.com/vostrok/mt_manager/src/service/instance"
+	"github.com/vostrok/pixels/src/notifier"
+)
+
+// does simple thing:
+// selects all subscriptions form database where pixel was not sent to publisher
+// and pushes to queue
+func AddPixelsHandler(r *gin.Engine) {
+	rg := r.Group("/api")
+	rg.GET("", api)
+}
+
+type Params struct {
+	Limit int
+	Hours int
+}
+
+func api(c *gin.Context) {
+	limitStr, _ := c.GetQuery("limit")
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil || limitStr == "" {
+		limit = svc.conf.service.Api.DefaultLimit
+	}
+
+	hoursStr, _ := c.GetQuery("hours")
+	hours, err := strconv.Atoi(hoursStr)
+	if err != nil || limitStr == "" {
+		hours = svc.conf.service.Api.DefaultBeforeHours
+	}
+
+	params := Params{
+		Limit: limit,
+		Hours: hours,
+	}
+
+	count, err := GetPixels(params)
+	if err != nil {
+		c.JSON(500, err.Error())
+		return
+	}
+	c.JSON(200, count)
+}
+
+// get not sent pixels
+func GetPixels(p Params) (int, error) {
+	var records []rec.Record
+
+	log.WithFields(log.Fields{
+		"count":  len(records),
+		"params": fmt.Sprintf("%#v", p),
+	}).Debug("get pixels")
+
+	begin := time.Now()
+	defer func() {
+		log.WithFields(log.Fields{
+			"took": time.Since(begin),
+		}).Debug("get pixels")
+	}()
+	query := fmt.Sprintf("SELECT "+
+		"tid, "+
+		"msisdn, "+
+		"id_campaign, "+
+		"id, "+
+		"operator_code, "+
+		"country_code, "+
+		"pixel, "+
+		"publisher "+
+		" FROM %ssubscriptions "+
+		" WHERE pixel != '' "+
+		" AND pixel_sent = false ",
+		svc.conf.db.TablePrefix)
+
+	if p.Hours > 0 {
+		query = query +
+			fmt.Sprintf(" AND (CURRENT_TIMESTAMP - %d * INTERVAL '1 hour' ) < created_at ", p.Hours)
+	}
+	query = query + fmt.Sprintf(" ORDER BY id DESC LIMIT %d", p.Limit)
+
+	rows, err := svc.db.Query(query)
+	if err != nil {
+		return 0, fmt.Errorf("db.Query: %s, query: %s", err.Error(), query)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		record := rec.Record{}
+
+		if err := rows.Scan(
+			&record.Tid,
+			&record.Msisdn,
+			&record.CampaignId,
+			&record.SubscriptionId,
+			&record.OperatorCode,
+			&record.CountryCode,
+			&record.Pixel,
+			&record.Publisher,
+		); err != nil {
+			return 0, fmt.Errorf("rows.Scan: %s", err.Error())
+		}
+		records = append(records, record)
+	}
+	if rows.Err() != nil {
+		return 0, fmt.Errorf("row.Err: %s", err.Error())
+	}
+
+	for _, v := range records {
+		go svc.n.PixelNotify(notifier.Pixel{
+			Tid:            v.Tid,
+			Msisdn:         v.Msisdn,
+			CampaignId:     v.CampaignId,
+			SubscriptionId: v.SubscriptionId,
+			OperatorCode:   v.OperatorCode,
+			CountryCode:    v.CountryCode,
+			Pixel:          v.Pixel,
+			Publisher:      v.Publisher,
+		})
+	}
+
+	return len(records), nil
+}
