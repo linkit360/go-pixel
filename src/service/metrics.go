5c8d3c2c93b9e3ee345748ae83b8abc9f1e69f13
Helena Nevskaya <helena.nevskaya@ya.ru>
2016-11-17 13:32:11 +0300

    metrics in minute interval

--- src/service/metrics.go
+++ src/service/metrics.go
@@ -1,27 +1,41 @@
 package service
 
 import (
-	"github.com/prometheus/client_golang/prometheus"
+	"time"
 
 	m "github.com/vostrok/metrics"
 )
 
 var (
-	dropped        prometheus.Counter
-	empty          prometheus.Counter
-	emptyPublisher prometheus.Counter
-	emptySettings  prometheus.Counter
-	publisherError prometheus.Counter
-	addToDBErrors  prometheus.Counter
-	addToDbSuccess prometheus.Counter
+	dropped        m.Gauge
+	empty          m.Gauge
+	emptyPublisher m.Gauge
+	emptySettings  m.Gauge
+	publisherError m.Gauge
+	addToDBErrors  m.Gauge
+	addToDbSuccess m.Gauge
 )
 
 func initMetrics() {
-	empty = m.NewCounter("empty", "Empty, invalid messages in queue")
-	dropped = m.NewCounter("dropped", "Dropped messages in queue")
-	emptyPublisher = m.NewCounter("empty_publisher", "Cannot determine publisher")
-	emptySettings = m.NewCounter("empty_settings", "No settings found for this publisher")
-	publisherError = m.NewCounter("publisher_error", "Request to publisher ended with error")
-	addToDBErrors = m.NewCounter("add_to_db_errors", "Any error connected with database occured")
-	addToDbSuccess = m.NewCounter("add_to_db_success", "Count of success")
+	empty = m.NewGauge("", "", "empty", "Empty, invalid messages in queue")
+	dropped = m.NewGauge("", "", "dropped", "Dropped messages in queue")
+	emptyPublisher = m.NewGauge("", "", "empty_publisher", "Cannot determine publisher")
+	emptySettings = m.NewGauge("", "", "empty_settings", "No settings found for this publisher")
+	publisherError = m.NewGauge("", "", "publisher_error", "Request to publisher ended with error")
+	addToDBErrors = m.NewGauge("", "", "add_to_db_errors", "Any error connected with database occured")
+	addToDbSuccess = m.NewGauge("", "", "add_to_db_success", "Count of success")
+
+	go func() {
+		// metrics in prometheus as for 15s (default)
+		// so make for minute interval
+		for range time.Tick(time.Minute) {
+			dropped.Update()
+			empty.Update()
+			emptyPublisher.Update()
+			emptySettings.Update()
+			publisherError.Update()
+			addToDBErrors.Update()
+			addToDbSuccess.Update()
+		}
+	}()
 }
