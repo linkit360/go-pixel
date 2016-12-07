738e3414ef6668819832a31433119c053f56c65a
Helena Nevskaya <helena.nevskaya@ya.ru>
2016-12-07 12:59:33 +0300

    config and compile

--- src/service/process.go
+++ src/service/process.go
@@ -34,7 +34,17 @@ type EventNotifyPixel struct {
 // operator=mobilink
 
 func processPixels(deliveries <-chan amqp.Delivery) {
+	var client *http.Client
+	var resp *http.Response
+
 	for msg := range deliveries {
+
+		var operator inmem_service.Operator
+		var ps inmem_service.PixelSetting
+		var cleintErr error
+		var err error
+		var statusCode int
+		var t notifier.Pixel
 
 		log.WithFields(log.Fields{
 			"body": string(msg.Body),
@@ -53,7 +63,7 @@ func processPixels(deliveries <-chan amqp.Delivery) {
 			goto ack
 		}
 
-		t := e.EventData
+		t = e.EventData
 		if t.Pixel == "" || t.Tid == "" {
 			dropped.Inc()
 			empty.Inc()
@@ -87,12 +97,12 @@ func processPixels(deliveries <-chan amqp.Delivery) {
 		}
 
 		// send pixel
-		ps := inmem_service.PixelSetting{
+		ps = inmem_service.PixelSetting{
 			Publisher:    t.Publisher,
 			OperatorCode: t.OperatorCode,
 			CampaignId:   t.CampaignId,
 		}
-		pixelSetting, err := inmem_client.GetPixelSettingByKeyWithRatio(ps.Key())
+		ps, err = inmem_client.GetPixelSettingByKeyWithRatio(ps.Key())
 		if err != nil {
 			err = fmt.Errorf("GetPixelSettingByKey: %s", err.Error())
 			dropped.Inc()
@@ -107,7 +117,7 @@ func processPixels(deliveries <-chan amqp.Delivery) {
 			}).Error("can't process pixel")
 			goto ack
 		}
-		if !pixelSetting.Enabled {
+		if !ps.Enabled {
 			dropped.Inc()
 
 			log.WithFields(log.Fields{
@@ -117,29 +127,29 @@ func processPixels(deliveries <-chan amqp.Delivery) {
 			}).Debug("send pixel disabled")
 			goto ack
 		}
-		if pixelSetting.SkipPixelSend {
+		if ps.SkipPixelSend {
 			dropped.Inc()
 			log.WithFields(log.Fields{
 				"pixel":   t.Pixel,
 				"tid":     t.Tid,
 				"msg":     "dropped",
 				"key":     ps.Key(),
-				"setting": fmt.Sprintf("%#v", pixelSetting),
+				"setting": fmt.Sprintf("%#v", ps),
 			}).Info("ratio: must skip")
 			goto ack
 		}
 		log.WithFields(log.Fields{
 			"pixel": t.Pixel,
 			"tid":   t.Tid,
-			"ratio": pixelSetting.Ratio,
-			"count": pixelSetting.Count,
+			"ratio": ps.Ratio,
+			"count": ps.Count,
 			"key":   ps.Key(),
 		}).Info("ratio rule: passed")
 
 		t.Sent = false
-		endpoint := ""
+
 		if svc.conf.server.Env == "dev" {
-			endpoint = "http://localhost:50309/publisher?" +
+			t.Endpoint = "http://localhost:50309/publisher?" +
 				"aff_sub=%pixel%&" +
 				"msisdn=%msisdn%&" +
 				"trxid=%trxid%&" +
@@ -147,15 +157,15 @@ func processPixels(deliveries <-chan amqp.Delivery) {
 				"country=%country_name%&" +
 				"operator=%operator_name%"
 		} else {
-			endpoint = pixelSetting.Endpoint
+			t.Endpoint = ps.Endpoint
 		}
 
-		endpoint = strings.Replace(endpoint, "%pixel%", t.Pixel, 1)
-		endpoint = strings.Replace(endpoint, "%msisdn%", t.Msisdn, 1)
-		endpoint = strings.Replace(endpoint, "%trxid%", fmt.Sprintf("%d%s", time.Now().Unix(), t.Msisdn), 1)
-		endpoint = strings.Replace(endpoint, "%time%", time.Now().UTC().Format("2006-01-02+15:04:05"), 1)
+		t.Endpoint = strings.Replace(t.Endpoint, "%pixel%", t.Pixel, 1)
+		t.Endpoint = strings.Replace(t.Endpoint, "%msisdn%", t.Msisdn, 1)
+		t.Endpoint = strings.Replace(t.Endpoint, "%trxid%", fmt.Sprintf("%d%s", time.Now().Unix(), t.Msisdn), 1)
+		t.Endpoint = strings.Replace(t.Endpoint, "%time%", time.Now().UTC().Format("2006-01-02+15:04:05"), 1)
 
-		operator, err := inmem_client.GetOperatorByCode(t.OperatorCode)
+		operator, err = inmem_client.GetOperatorByCode(t.OperatorCode)
 		if err != nil {
 			err = fmt.Errorf("GetPixelSettingByKey: %s", err.Error())
 			dropped.Inc()
@@ -168,19 +178,18 @@ func processPixels(deliveries <-chan amqp.Delivery) {
 			}).Error("can't get operator name by code")
 			goto ack
 		}
-		endpoint = strings.Replace(endpoint, "%operator_name%", operator.Name, 1)
-		endpoint = strings.Replace(endpoint, "%country_name%", operator.CountryName, 1)
+		t.Endpoint = strings.Replace(t.Endpoint, "%operator_name%", operator.Name, 1)
+		t.Endpoint = strings.Replace(t.Endpoint, "%country_name%", operator.CountryName, 1)
 
-		client := &http.Client{
-			Timeout: time.Duration(pixelSetting.Timeout) * time.Second,
+		client = &http.Client{
+			Timeout: time.Duration(ps.Timeout) * time.Second,
 		}
 
-		resp, cleintErr := client.Get(endpoint)
+		resp, cleintErr = client.Get(t.Endpoint)
 		if cleintErr != nil || resp.StatusCode != 200 {
 			publisherError.Inc()
 		}
 
-		var statusCode int
 		if resp != nil {
 			statusCode = resp.StatusCode
 			t.ResponseCode = resp.StatusCode
@@ -189,7 +198,7 @@ func processPixels(deliveries <-chan amqp.Delivery) {
 			"pixel":    t.Pixel,
 			"tid":      t.Tid,
 			"campaign": t.CampaignId,
-			"url":      endpoint,
+			"url":      t.Endpoint,
 			"code":     statusCode,
 		}).Debug("response")
 
